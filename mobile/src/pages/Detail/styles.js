ade5ccb75f494152d5e975decde7b98a2359b66e
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2020-04-06 15:10:03 -0300

    instalando eslint e ajustando arquivos

--- mobile/src/pages/Detail/styles.js
+++ mobile/src/pages/Detail/styles.js
@@ -2,77 +2,77 @@ import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
-    },
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
 
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 
-    incident: {
-        padding: 24,
-        borderRadius: 8,
-        backgroundColor: '#FFFFFF',
-        marginTop: 48,
-        marginBottom: 16
-    },
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFFFFF',
+    marginTop: 48,
+    marginBottom: 16,
+  },
 
-    incidentProperty: {
-        fontSize: 14,
-        color: '#41414d',
-        fontWeight: 'bold',
-        marginTop: 24
-    },
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold',
+    marginTop: 24,
+  },
 
-    incidentValue: {
-        marginTop: 8,
-        fontSize: 15,
-        color: '#737380'
-    },
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380',
+  },
 
-    contactBox: {
-        padding: 24,
-        borderRadius: 8,
-        backgroundColor: '#FFFFFF',
-        marginBottom: 16
-    },
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFFFFF',
+    marginBottom: 16,
+  },
 
-    heroTitle: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#13131a',
-        lineHeight: 30
-    },
+  heroTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
 
-    heroDescription: {
-        fontSize: 15,
-        color: '#737380',
-        marginTop: 16
-    },
+  heroDescription: {
+    fontSize: 15,
+    color: '#737380',
+    marginTop: 16,
+  },
 
-    actions: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 16
-    },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
+  },
 
-    action: {
-        backgroundColor: '#E02041',
-        borderRadius: 8,
-        height: 50,
-        width: '48%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+  action: {
+    backgroundColor: '#E02041',
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    actionText: {
-        color: '#FFFFFF',
-        fontSize: 15,
-        fontWeight: 'bold'
-    }
+  actionText: {
+    color: '#FFFFFF',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
 });
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -7,12 +7,12 @@ import Detail from './pages/Detail';
 const AppStack = createStackNavigator();
 
 export default function Routes() {
-    return (
-        <NavigationContainer >
-            <AppStack.Navigator screenOptions={{ headerShown: false }}>
-                <AppStack.Screen name="Incidents" component={Incidents} />
-                <AppStack.Screen name="Detail" component={Detail} />
-            </AppStack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 }
